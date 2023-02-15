4ebc13522db80602290e29245343f7366d3ad21d
Kilari Teja <kilariteja9@gmail.com>
2023-02-16 02:58:30 +0530

    Drop decode before proxy console

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Decode } from "console-feed/lib/Transform"
 import Hook from "console-feed/lib/Hook"
 import Bus from "./bus"
 
@@ -10,10 +9,7 @@ bus.listen("ping", () => bus.emit("pong", null))
 // Navigation helpers.
 bus.listen("history.back", () => window.history.back())
 bus.listen("history.forward", () => window.history.forward())
-bus.listen("location.reload", () => {
-    console.log("relodaing....")
-    window.location.reload()
-})
+bus.listen("location.reload", () => window.location.reload())
 
 // Console proxy.
-// Hook(window.console, (log) => bus.emit("console-feed.message", Decode(log)))
+Hook(window.console, (log) => bus.emit("console-feed.message", log))
