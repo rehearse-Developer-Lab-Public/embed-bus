1f0c36a2850c63848494a4eb7229ca2be698a23b
Kilari Teja <kilariteja9@gmail.com>
2023-02-16 03:12:15 +0530

    Drop debug ticks

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,4 @@ bus.listen("history.forward", () => window.history.forward())
 bus.listen("location.reload", () => window.location.reload())
 
 // Console proxy.
-Hook(window.console, (log) => {
-    alert("Sorry")
-    bus.emit("console-feed.message", log)
-})
-
-setInterval(() => {
-    bus.emit("tick", null)
-}, 2500)
+Hook(window.console, (log) => bus.emit("console-feed.message", log))
